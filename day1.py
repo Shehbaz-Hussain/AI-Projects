a800a41f2bafe6c9dbf05494cbf46ddf6f9eeaf3
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-06-24 03:08:19 +0500

    Day1: Variables, Input, Print practice

---
+++ day1.py
@@ -0,0 +1,31 @@
+# Day 1: Variables, Input, and Print
+
+
+# Variables Example
+
+name = "Shehbaz Hussain"
+age = 20
+
+print("Name:", name)
+print("Age:", age)
+
+
+# User Profile Input
+
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+
+
+# Display Profile
+
+print("\n--- PROFILE ---")
+print("Name:", user_name)
+print("Age:", user_age)
+print("City:", city)
+
+
+# Age Future Calculation
+
+print("\nAge after 5 years:", user_age + 5)
+print("Age after 10 years:", user_age + 10)
