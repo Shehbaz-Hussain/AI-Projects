bf5ece5d63601c43c6a5b7c020ff8fec811d3d80
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-06-29 05:44:10 +0500

    Initial commit: Hello World Python setup

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+# hell0_world.py - My very first python file
+# Shehbaz AI Systems - AI Engineering Journey
+
+print("Hello AI World")
+print("My AI Journey begins today.")
+print("Shehbaz AI Systems - Building one step at a Time.")
