f1ecea87504cfcdbabea74c88957696f6b452458
Shehbaz Hussain <shehbazhussain1to10@gmail.com>
2026-07-01 04:02:23 +0500

    hello_world update

--- hello_world.py
+++ hello_world.py
@@ -4,3 +4,10 @@
 print("Hello AI World")
 print("My AI Journey begins today.")
 print("Shehbaz AI Systems - Building one step at a Time.")
+
+# Basic operations
+
+print(2 + 8)
+print(6 - 3)
+print(3 * 34)
+print(89 / 5)
